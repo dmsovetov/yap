d9cd1299b29272d572b46a64bb99b83515851cb7
Dmitry Sovetov <DSovetov@dm.local>
2015-02-24 10:49:57 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@
 from setuptools import setup, find_packages
 
 setup(name='yap',
-      version='0.5.4',
+      version='0.5.5',
       description='yap is a yet another project generator.',
       url='https://github.com/dmsovetov/yap',
       scripts=['bin/yap'],
